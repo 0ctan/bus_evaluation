75ca325fe6a6ab822ed066870117dbe554c05dd1
0ctan <octan@sfc.wide.ad.jp>
2020-01-28 07:17:23 +0900

    バス停範囲作った

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # bus_evaluation
 卒論評価取り
+## usage 
+
+$python killnull.py data/hoge.csv
+$python stopdetect.py
+
+
 
 ## データの取得
 mongoexport -d bus_location -c keiiku_A --out 20200102.csv --type csv --fields "time,lat,lon"
@@ -9,3 +15,4 @@ mongoexport -d bus_location -c keiiku_A --out 20200102.csv --type csv --fields "
 ## 有効ログ
 書き日時以降
 2019-12-09T08:45:02
+
--- stopdetect.py
+++ stopdetect.py
@@ -6,7 +6,7 @@ args = sys.argv
 
 file = args[1]
 
-filename = pd.read_csv(file)
+f = pd.read_csv(file)
 
 # 配列に格納
 lyear = []
@@ -18,25 +18,49 @@ lsec = []
 llat = []
 llon = []
 
-lyear= filename['year']
-lmonth= filename['month']
-lday = filename['day']
-lh = filename['h']
-lmin = filename['min']
-lsec = filename['sec']
-llat = filename['lat']
-llon = filename['lon']
+lyear= f['year']
+lmonth= f['month']
+lday = f['day']
+lh = f['h']
+lmin = f['min']
+lsec = f['sec']
+llat = f['lat']
+llon = f['lon']
 
 length = len(lyear)
-timeout = 0
+
 
 # 基準は+-0.0002以内かどうか
+busstopnow = list()
 
+global keiikuflag
+global yokadoflag
+global shonanflag
 
 for n in range(length):
-    if 23 <= h[n] or h[n]<8 : # 08:00:00〜16:59:59(JST)まで有効コードはUTC表記
-        if 35.3076<listlat[n]<35.3080 : # 慶育病院
-            if 139.4313<listlon[n]<139.4317 :
+    if 23 <= lh[n] or lh[n]<8 : # 08:00:00〜16:59:59(JST)まで有効コードはUTC表記
+        if 35.3076<=llat[n]<=35.3080 : # 慶育病院
+            if 139.4313<=llon[n]<=139.4317 :
                 # 慶育判定
-                timeout =　
+                time = datetime.datetime(lyear[n], lmonth[n], lday[n], lh[n], lmin[n], lsec[n])
+                if keiikuflag < time:
+                    # フラグなかったとき
+                    busstopnow.append([lyear[n], lmonth[n], lday[n], lh[n], lmin[n], lsec[n], "keiiku"]) # リストに追加
+                    keiikuflag = time + datetime.timedelta(minutes=25) # 25分後にフラグを設定
+        if 35.3914<=llat[n]<=35.3918 : # よーかどー
+            if 139.4471<=llon[n]<=139.4475 :
+                # ヨーカどー判定
+                time = datetime.datetime(lyear[n], lmonth[n], lday[n], lh[n], lmin[n], lsec[n])
+                if yokadoflag < time:
+                    # フラグなかったとき
+                    busstopnow.append([lyear[n], lmonth[n], lday[n], lh[n], lmin[n], lsec[n], "yokado"]) # リストに追加
+                    yokadoflag =　time + datetime.timedelta(minutes=25)　 # 25分後にフラグを設定
+        if 35.3951<=llat[n]<=35.3955 : # 湘南台
+            if 139.4662<=llon[n]<=139.4666 :
+                # 湘南台判定
+                time = datetime.datetime(lyear[n], lmonth[n], lday[n], lh[n], lmin[n], lsec[n])
+                if shonanflag < time:
+                    # フラグなかったとき
+                    busstopnow.append([lyear[n], lmonth[n], lday[n], lh[n], lmin[n], lsec[n], "shonan"]) # リストに追加
+                    shonanflag =　time + datetime.timedelta(minutes=25)　 # 25分後にフラグを設定
             
