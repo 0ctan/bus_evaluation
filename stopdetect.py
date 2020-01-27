e76de42671e0f2aa0853c05ac3bc5890d966fc8e
0ctan <octan@sfc.wide.ad.jp>
2020-01-28 05:33:26 +0900

    時間判定

--- stopdetect.py
+++ stopdetect.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sys
+import 
 
 args = sys.argv
 
@@ -8,22 +9,34 @@ file = args[1]
 filename = pd.read_csv(file)
 
 # 配列に格納
-listTime = []
-listlat = []
-listlon = []
+lyear = []
+lmonth = []
+lday = []
+lh = []
+lmin = []
+lsec = []
+llat = []
+llon = []
 
-listTime = filename['time']
-listlat = filename['lat']
-listlon = filename['lon']
+lyear= filename['year']
+lmonth= filename['month']
+lday = filename['day']
+lh = filename['h']
+lmin = filename['min']
+lsec = filename['sec']
+llat = filename['lat']
+llon = filename['lon']
 
-length = len(listTime)
+length = len(lyear)
 timeout = 0
 
 # 基準は+-0.0002以内かどうか
 
+
 for n in range(length):
-    if 35.3076<listlat[n]<35.3080 : # 慶育病院
-        if 139.4313<listlon[n]<139.4317 :
-            # 慶育判定
-            timeout =
-        
+    if 23 <= h[n] or h[n]<8 : # 08:00:00〜16:59:59(JST)まで有効コードはUTC表記
+        if 35.3076<listlat[n]<35.3080 : # 慶育病院
+            if 139.4313<listlon[n]<139.4317 :
+                # 慶育判定
+                timeout =　
+            
