33617d57747bb5f2298265c558dcae5ce5b03d9a
0ctan <octan@sfc.wide.ad.jp>
2020-02-01 01:28:53 +0900

    バカバグ修正

--- evaluation.py
+++ evaluation.py
@@ -9,10 +9,23 @@ file = "data/"+args[1]+"_etatable.csv"
 f = pd.read_csv(file, header=None)
 
 # 絞り込み
-use = f[[3,8,9]]
-use[9] = pd.to_timedelta(use[9])
-use[9] = use[9].dt.total_seconds()
-weekday_bin = use.groupby([3,8])
+use = f[[0,1,2]]
+# use[2] = pd.to_timedelta(use[2]) 
+# use[2]は秒数での00:00:00からの経過時間が入っている
+weekday_bin = use.groupby([0,1])
+
+# 時刻表データ
+tt = pd.read_csv("data/timetable.csv")
+# 配列に入れる
+ttbin = []
+ttyokado = []
+ttshonandai = []
+ttkeiiku = []
+
+ttbin = tt['bin']
+ttyokado = tt['yokado']
+ttshonandai = tt['shonandai']
+ttkeiiku = tt['keiiku']
 
 # print(weekday_bin.groups)
 # mean = weekday_bin.mean()
@@ -25,21 +38,25 @@ file2 = "data/eva"+args[1]+".csv"
 f2 = pd.read_csv(file2, header=None)
 eva = f2[[3,8,9]]
 eva[9] = pd.to_timedelta(eva[9])
-eva[9] = eva[9].dt.total_seconds()
-weekday_bin_eva = eva.groupby([3,8])
+eva[9] = eva[9].dt.total_seconds() # 時間を秒単位に変換
+consyu = eva.groupby([3,8])
 
 youbi = 0
 evaluation = list()
+aho = "tt"+args[1]
+str(aho)
 
 while youbi <= 4:
     #曜日ごと
     binnmei = 0
     while binnmei <= 12:
         # 便ごと
+        table = aho[binnmei] #tableには秒数で絶対時刻が入ってる
         binnmei = binnmei + 1
         tmpbin = str(binnmei)
         target = "A"+tmpbin
-        kekka = weekday_bin_eva.loc[youbi, target] - weekday_bin.loc[youbi, target] # kekkaは秒数、時刻表からのプラマイで入ってる。小さほど正義
+        # consyuがプラマイ秒でweekday_binが秒の絶対時刻。そのためtableを＋してkekkaには差分だけ入れる
+        kekka = consyu.loc[youbi, target] - weekday_bin.loc[youbi, target] + table # kekkaは秒数、時刻表からのプラマイで入ってる。小さほど正義
         evaluation.append([youbi, target, kekka])
     youbi = youbi + 1
     
